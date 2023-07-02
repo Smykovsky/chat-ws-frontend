f9a02601becfe208feb9a9d61bbb7ec6d0f24d4f
Smyku <kamil.smyk00@gmail.com>
2023-07-02 13:58:05 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
+    <input v-model="credentials.value" type="text" placeholder="Wprowadź wiadomość" />
+    <button @click="sendMessage">Wyślij</button>
     <ul>
       <li v-for="(message, index) in messages" :key="index">
         {{ message }}
       </li>
     </ul>
-    <input v-model="credentials.value" type="text" placeholder="Wprowadź wiadomość" />
-    <input v-model="credentials.user" type="text" placeholder="Wprowadź wiadomość" />
-    <button @click="sendMessage">Wyślij</button>
   </div>
 </template>
 
@@ -18,59 +17,27 @@ export default {
       messages: [],
       credentials: {
         value: '',
-        user: ''
       }
     }
   },
   mounted() {
     this.$stompClient.onConnect = (frame) => {
       console.log("połączono!")
-      this.$stompClient.subscribe('/topic/messages', (message) => {
-        console.log(JSON.parse(message.body).value, JSON.parse(message.body).user)
-      })
-
-        // const messageToSend = {
-        //   value: 'wiadomość!',
-        //   user: 'Smyku'
-        // }
-        // const destination = '/app/chat' // Adres docelowy na serwerze
-        // this.$stompClient.publish({ destination, body: JSON.stringify(messageToSend) })
-        // this.messages.push(messageToSend.value)
+      this.$stompClient.subscribe('/topic/messages', this.onMessageReceived)
     }
-
-    this.$stompClient.onStompError = (frame) => {
-      console.log("Błąd podczas łączenia!")
-    }
-
     this.$stompClient.activate()
   },
   methods: {
     sendMessage() {
       this.$stompClient.publish({destination: "/app/chat", body: JSON.stringify(this.credentials)})
-      this.messages.push(this.credentials.value)
       this.credentials.value = ''
-      this.credentials.user = ''
-    }
-  }
+    },
+    onMessageReceived(stompMessage) {
 
-  // mounted() {
-  //   this.connection = new WebSocket("ws://localhost:8080/chat")
-  //
-  //   this.connection.onopen = function (event) {
-  //     console.log(event)
-  //     console.log("succes")
-  //   }
-  //
-  //   this.connection.onmessage = function (event) {
-  //     console.log(event)
-  //     this.messages.push(event.data)
-  //   }
-  // },
-  // methods: {
-  //   sendMessage(message) {
-  //     this.connection.send(message);
-  //   }
-  // }
+      const message = JSON.parse(stompMessage.body).value;
+      this.messages.push(message);
+    },
+  }
 }
 </script>
 
