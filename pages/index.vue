f57d493bcb145081a7b274cbe722335637ef69f5
Smykovsky <kamil.smyk00@gmail.com>
2023-09-14 14:05:35 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <div class="register">
-      <input id="user-name" v-model="credentials.username">
+      <input id="user-name" v-model="username">
       <button type="button" @click="registerUser">Połącz</button>
     </div>
 
     <input v-model="credentials.value" type="text" placeholder="Wprowadź wiadomość" />
     <button @click="sendMessage">Wyślij</button>
     <ul>
-      <li v-for="(message, index) in messages" :key="index">
+      <li v-for="(message, index) in publicChat" :key="index">
         {{ message }}
       </li>
     </ul>
@@ -20,8 +20,8 @@ export default {
   data() {
     return {
       messages: [],
+      username: '',
       credentials: {
-        username: '',
         senderName: '',
         receiverName: '',
         value: '',
@@ -31,48 +31,60 @@ export default {
     }
   },
   mounted() {
-    this.$stompClient.onConn(this.onMessageReceived, this.onPrivateMessageReceived, this.credentials.username);
-
-    this.$stompClient.connect()
 
   },
   methods: {
-    sendMessage() {
-      this.$stompClient.publish({destination: "/app/chat", body: JSON.stringify(this.credentials)})
-      this.credentials.value = ''
-    },
-    onMessageReceived(stompMessage) {
-      const message = JSON.parse(stompMessage.body).value;
-      this.messages.push(message);
-      console.log(message)
-    },
     connect() {
-      console.log(this.$stompClient)
+      this.$stompClient.connect({}, this.onConnected(), this.onError());
     },
-    registerUser() {
-      this.connect();
+    onConnected() {
+      this.$stompClient.subscribe('/chatroom/public', this.onMessageReceived)
+      this.$stompClient.subscribe('/user'+this.username+'/private', this.onPrivateMessageReceived)
+      console.log(this.$stompClient.connected)
+      this.userJoin()
+    },
+    onError(error) {
+      console.log(error)
+    },
+    onMessageReceived(payload) {
+      let payloadData = JSON.parse(payload.body)
+      this.publicChat.push(payloadData)
+      console.log(this.publicChat)
+    },
+    onPrivateMessageReceived(payload) {
+      var payloadData = JSON.parse(payload.body)
+      if (this.privateChats.get(payloadData.senderName)) {
+        this.privateChats.get(payloadData.senderName).push(payloadData)
+        this.privateChats.set(new Map(this.privateChats))
+      } else {
+        let list = []
+        list.push(payloadData)
+        this.privateChats.set(payloadData.senderName, list);
+        this.privateChats(new Map(this.privateChats))
+      }
     },
     userJoin() {
       var chatMessage = {
-        senderName: this.credentials.username
+        senderName: this.credentials.senderName
       };
-      this.$stompClient.send("/app/chat", {}, JSON.stringify(chatMessage))
-
+      this.$stompClient.send("/app/message",  JSON.stringify(chatMessage))
+      console.log("Cześć " + this.username)
     },
-    onPrivateMessageReceived(payload) {
-        var payloadData = JSON.parse(payload.body);
-        if (this.privateChats.get(payloadData.senderName)){
-          this.privateChats.get(payloadData.senderName).push(payloadData);
-          this.privateChats.set(new Map(this.privateChats));
-        } else {
-          let list = [];
-          list.push(payloadData);
-          this.privateChats.set(payloadData.senderName, list);
-          this.privateChats.set(new Map(this.privateChats));
-        }
+    registerUser() {
+      this.connect()
+    },
+    sendMessage() {
+      // if (this.$stompClient) {
+      //   var chatMessage = {
+      //     senderName: this.credentials.senderName,
+      //     receiverName: this.credentials.receiverName,
+      //     value: this.credentials.value
+      //   };
+      //
+      //   this.$stompClient.send("/app/message",  JSON.stringify(chatMessage))
+      // }
+      console.log(this.$stompClient.connected)
     }
-
-
   }
 }
 </script>
--- plugins/socket.js
+++ plugins/socket.js
@@ -1,88 +1,9 @@
 import Vue from "vue";
-import { Client } from "@stomp/stompjs";
+import SockJS from 'sockjs-client'
+import Stomp from 'webstomp-client'
 
+const sock = new SockJS('http://localhost:8080/ws')
+const stompClient = Stomp.over(sock)
 
-export default ({ app }) => {
-  const socket = new WebSocket('ws://localhost:8080/chat')
-  const stompClient = new Client()
+Vue.prototype.$stompClient = stompClient
 
-  stompClient.webSocketFactory = () => socket
-
-  stompClient.connect = () => {
-    stompClient.activate()
-  }
-
-  stompClient.onConn = (onReceived, onPrivateReceived, username) => {
-    stompClient.onConnect = () => {
-      stompClient.subscribe('/chatroom/messages', onReceived);
-      stompClient.subscribe('/user/'+username+'/private', onPrivateReceived);
-      console.log('poloczono')
-    }
-  }
-
-  // stompClient.onConnect = (frame) => {
-  //   console.log("połączono!")
-  //   stompClient.subscribe('/topic/messages', (message) => {
-  //     console.log('Otrzymano wiadomość!', message.body)
-  //   })
-  //
-  //   const messageToSend = {
-  //     value: 'wiadomość!',
-  //     user: 'Smyku'
-  //   }
-  //   const destination = '/app/chat' // Adres docelowy na serwerze
-  //   stompClient.publish({ destination, body: JSON.stringify(messageToSend) })
-  //
-  // }
-  // // Opcjonalnie, dodaj obsługę zdarzenia błędu
-  // stompClient.onStompError = (frame) => {
-  //   console.error('Błąd STOMP:', frame)
-  // }
-  //
-  //
-  //
-  //
-  // // Połącz się z serwerem STOMP
-  // stompClient.activate()
-
-
-  // // Obsługa zdarzenia połączenia
-  // socket.onopen = (data) => {
-  //   console.log('Połączono z serwerem WebSocket')
-  //   console.log(data)
-  //
-  //   const subscriptionMessage = {
-  //     commad: 'subscribe',
-  //     destination: '/topic/messages'
-  //   }
-  //   socket.send(JSON.stringify(subscriptionMessage))
-  //   // Tutaj możesz wykonać dodatkowe czynności po nawiązaniu połączenia
-  // }
-  //
-  // // Obsługa zdarzenia otrzymania wiadomości
-  // socket.onmessage = (event) => {
-  //   const message = event.data
-  //   console.log('Otrzymano wiadomość:', message)
-  //   // Tutaj możesz obsłużyć otrzymaną wiadomość
-  // }
-  //
-  // // Opcjonalnie, dodaj obsługę zdarzenia błędu
-  // socket.onerror = (error) => {
-  //   console.error('Błąd WebSocket:', error)
-  // }
-  //
-  // // Opcjonalnie, dodaj obsługę zdarzenia zamknięcia połączenia
-  // socket.onclose = (event) => {
-  //   console.log('Zamknięto połączenie WebSocket:', event)
-  // }
-  //
-  // // Opcjonalnie, dodaj obsługę innych zdarzeń WebSocket
-  //
-  // // Dodaj event listener, aby zamknąć połączenie przed zamknięciem aplikacji
-  // app.beforeUnmount(() => {
-  //   socket.close()
-  // })
-
-  // Dodaj instancję WebSocket do obiektu Vue
-  Vue.prototype.$stompClient = stompClient
-}
