6d95c530d248f5e83ee2aa808e6b3d09aa1d0c29
kamil.smyk <kamil.smyk00@gmail.com>
2023-09-15 09:58:11 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,94 +1,196 @@
 <template>
-  <div>
-    <div class="register">
-      <input id="user-name" v-model="username">
-      <button type="button" @click="registerUser">Połącz</button>
+  <div class="container">
+    <div v-if="userData.connected" class="chat-box">
+      <div class="member-list">
+        <ul>
+          <li @click="setTab('CHATROOM')" :class="{ active: tab === 'CHATROOM' }">Chatroom</li>
+          <li
+            @click="setTab(name)"
+            :class="{ active: tab === name }"
+            v-for="(name, index) in onlineUsers"
+            :key="index"
+          >{{ name }}</li>
+        </ul>
+      </div>
+      <div v-if="tab === 'CHATROOM'" class="chat-content">
+        <ul class="chat-messages">
+          <li
+            class="message"
+            :class="{ self: chat.senderName === userData.username }"
+            v-for="(chat, index) in publicChats"
+            :key="index"
+          >
+            <div v-if="chat.senderName !== userData.username" class="avatar">{{ chat.senderName }}</div>
+            <div class="message-data">{{ chat.message }}</div>
+            <div v-if="chat.senderName === userData.username" class="avatar self">{{ chat.senderName }}</div>
+          </li>
+        </ul>
+        <div class="send-message">
+          <input type="text" class="input-message" placeholder="enter the message" v-model="userData.message" @input="handleMessage" />
+          <button type="button" class="send-button" @click="sendValue">send</button>
+        </div>
+      </div>
+      <div v-else class="chat-content">
+        <ul class="chat-messages">
+          <li
+            class="message"
+            :class="{ self: chat.senderName === userData.username }"
+            v-for="(chat, index) in privateChats[tab]"
+            :key="index"
+          >
+            <div v-if="chat.senderName !== userData.username" class="avatar">{{ chat.senderName }}</div>
+            <div class="message-data">{{ chat.message }}</div>
+            <div v-if="chat.senderName === userData.username" class="avatar self">{{ chat.senderName }}</div>
+          </li>
+        </ul>
+        <div class="send-message">
+          <input type="text" class="input-message" placeholder="enter the message" v-model="userData.message" @input="handleMessage" />
+          <button type="button" class="send-button" @click="sendPrivateValue">send</button>
+        </div>
+      </div>
     </div>
-
-    <input v-model="credentials.value" type="text" placeholder="Wprowadź wiadomość" />
-    <button @click="sendMessage">Wyślij</button>
-    <ul>
-      <li v-for="(message, index) in publicChat" :key="index">
-        {{ message }}
-      </li>
-    </ul>
+    <div v-else class="register">
+      <input
+        id="user-name"
+        placeholder="Enter your name"
+        name="userName"
+        v-model="userData.username"
+        @input="handleUsername"
+        margin="normal"
+      />
+      <button type="button" @click="registerUser">
+        connect
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+import SockJS from 'sockjs-client';
+import { over } from 'stompjs';
+import { EventBus } from "~/plugins/eventBus";
+
 export default {
   data() {
     return {
-      messages: [],
-      username: '',
-      credentials: {
-        senderName: '',
-        receiverName: '',
-        value: '',
-      },
       privateChats: new Map(),
-      publicChat: [],
-    }
-  },
-  mounted() {
-
+      publicChats: [],
+      tab: 'CHATROOM',
+      userData: {
+        username: '',
+        receiverName: '',
+        connected: false,
+        message: '',
+      },
+      stompClient: null,
+      onlineUsers: []
+    };
   },
   methods: {
     connect() {
-      this.$stompClient.connect({}, this.onConnected(), this.onError());
+      const Sock = new SockJS('http://localhost:8080/ws');
+      this.stompClient = over(Sock);
+      this.stompClient.connect({}, this.onConnected, this.onError);
     },
     onConnected() {
-      this.$stompClient.subscribe('/chatroom/public', this.onMessageReceived)
-      this.$stompClient.subscribe('/user'+this.username+'/private', this.onPrivateMessageReceived)
-      console.log(this.$stompClient.connected)
-      this.userJoin()
-    },
-    onError(error) {
-      console.log(error)
-    },
-    onMessageReceived(payload) {
-      let payloadData = JSON.parse(payload.body)
-      this.publicChat.push(payloadData)
-      console.log(this.publicChat)
-    },
-    onPrivateMessageReceived(payload) {
-      var payloadData = JSON.parse(payload.body)
-      if (this.privateChats.get(payloadData.senderName)) {
-        this.privateChats.get(payloadData.senderName).push(payloadData)
-        this.privateChats.set(new Map(this.privateChats))
-      } else {
-        let list = []
-        list.push(payloadData)
-        this.privateChats.set(payloadData.senderName, list);
-        this.privateChats(new Map(this.privateChats))
-      }
+      this.userData.connected = true;
+      this.stompClient.subscribe('/chatroom/public', this.onMessageReceived);
+      this.stompClient.subscribe('/user/' + this.userData.username + '/private', this.onPrivateMessage);
+      this.userJoin();
     },
     userJoin() {
-      var chatMessage = {
-        senderName: this.credentials.senderName
+      const chatMessage = {
+        senderName: this.userData.username,
+        status: 'JOIN',
       };
-      this.$stompClient.send("/app/message",  JSON.stringify(chatMessage))
-      console.log("Cześć " + this.username)
+      this.stompClient.send('/app/message', {}, JSON.stringify(chatMessage));
+    },
+    onMessageReceived(payload) {
+      const payloadData = JSON.parse(payload.body);
+      switch (payloadData.status) {
+        case 'JOIN':
+          if (!this.privateChats[payloadData.senderName]) {
+            this.$set(this.privateChats, payloadData.senderName, []);
+          }
+          if (!this.onlineUsers.includes(payloadData.senderName)) {
+            this.onlineUsers.push(payloadData.senderName);
+            EventBus.$emit('updateOnlineUsers', this.onlineUsers);
+          }
+          break;
+        case 'MESSAGE':
+          this.publicChats.push(payloadData);
+          break;
+      }
+    },
+    onPrivateMessage(payload) {
+      const payloadData = JSON.parse(payload.body);
+      if (this.privateChats[payloadData.senderName]) {
+        this.privateChats[payloadData.senderName].push(payloadData);
+      } else {
+        this.$set(this.privateChats, payloadData.senderName, [payloadData]);
+      }
+      EventBus.$emit('updatePrivateChats', this.privateChats);
+    },
+    onError(err) {
+      console.log(err);
+    },
+    handleMessage(event) {
+      this.userData.message = event.target.value;
+    },
+    sendValue() {
+      if (this.stompClient) {
+        const chatMessage = {
+          senderName: this.userData.username,
+          message: this.userData.message,
+          status: 'MESSAGE',
+        };
+        this.stompClient.send('/app/message', {}, JSON.stringify(chatMessage));
+        this.userData.message = '';
+      }
+    },
+    sendPrivateValue() {
+      if (this.stompClient) {
+        const chatMessage = {
+          senderName: this.userData.username,
+          receiverName: this.tab,
+          message: this.userData.message,
+          status: 'MESSAGE',
+        };
+
+        if (this.userData.username !== this.tab) {
+          if (!this.privateChats[this.tab]) {
+            this.$set(this.privateChats, this.tab, []);
+          }
+          this.privateChats[this.tab].push(chatMessage);
+        }
+        this.stompClient.send('/app/private-message', {}, JSON.stringify(chatMessage));
+        this.userData.message = '';
+      }
+    },
+    handleUsername(event) {
+      this.userData.username = event.target.value;
     },
     registerUser() {
-      this.connect()
+      this.connect();
     },
-    sendMessage() {
-      // if (this.$stompClient) {
-      //   var chatMessage = {
-      //     senderName: this.credentials.senderName,
-      //     receiverName: this.credentials.receiverName,
-      //     value: this.credentials.value
-      //   };
-      //
-      //   this.$stompClient.send("/app/message",  JSON.stringify(chatMessage))
-      // }
-      console.log(this.$stompClient.connected)
-    }
-  }
-}
+    setTab(name) {
+      this.tab = name;
+    },
+  },
+  created() {
+    EventBus.$on('updatePrivateChats', (privateChats) => {
+      this.privateChats = privateChats;
+    });
+    EventBus.$on('updatePublicChats', (publicChats) => {
+      this.publicChats = publicChats;
+    });
+    EventBus.$on('updateOnlineUsers', (onlineUsers) => {
+      this.onlineUsers = onlineUsers; // Aktualizujemy listę online users
+    });
+  },
+};
 </script>
 
-<style>
-
+<style lang="css">
+@import "assets/index.css";
 </style>
--- plugins/socket.js
+++ plugins/socket.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import SockJS from 'sockjs-client'
-import Stomp from 'webstomp-client'
-
-const sock = new SockJS('http://localhost:8080/ws')
-const stompClient = Stomp.over(sock)
-
-Vue.prototype.$stompClient = stompClient
-
+// import Vue from "vue";
+// import SockJS from 'sockjs-client'
+// import Stomp from 'webstomp-client'
+//
+// const sock = new SockJS('http://localhost:8080/ws')
+// const stompClient = Stomp.over(sock)
+//
+// Vue.prototype.$stompClient = stompClient
+//
