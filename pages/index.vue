72a381588fd55ff22767052c8d35c766d4e7c524
Smykovsky <kamil.smyk00@gmail.com>
2023-09-18 19:12:54 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -95,7 +95,7 @@ export default {
     onConnected() {
       this.userData.connected = true;
       this.stompClient.subscribe('/chatroom/public', this.onMessageReceived);
-      this.stompClient.subscribe('/user/' + this.userData.username + '/private', this.onPrivateMessage);
+      this.stompClient.subscribe('/user/' + this.userData.username + '/private', this.onPrivateMessageReceived);
       this.userJoin();
     },
     userJoin() {
@@ -111,10 +111,12 @@ export default {
         case 'JOIN':
           if (!this.privateChats[payloadData.senderName]) {
             this.$set(this.privateChats, payloadData.senderName, []);
+            console.log("Witaj: " + payloadData.senderName)
           }
           if (!this.onlineUsers.includes(payloadData.senderName)) {
             this.onlineUsers.push(payloadData.senderName);
             EventBus.$emit('updateOnlineUsers', this.onlineUsers);
+            console.log("Dodaje nowego użytkownika: " + payloadData.senderName)
           }
           break;
         case 'MESSAGE':
@@ -122,7 +124,7 @@ export default {
           break;
       }
     },
-    onPrivateMessage(payload) {
+    onPrivateMessageReceived(payload) {
       const payloadData = JSON.parse(payload.body);
       if (this.privateChats[payloadData.senderName]) {
         this.privateChats[payloadData.senderName].push(payloadData);
@@ -130,6 +132,8 @@ export default {
         this.$set(this.privateChats, payloadData.senderName, [payloadData]);
       }
       EventBus.$emit('updatePrivateChats', this.privateChats);
+      EventBus.$emit('updateOnlineUsers', this.onlineUsers)
+      console.log("otrzymano private message!")
     },
     onError(err) {
       console.log(err);
@@ -180,12 +184,18 @@ export default {
   created() {
     EventBus.$on('updatePrivateChats', (privateChats) => {
       this.privateChats = privateChats;
+      console.log("Aktualizuje private chats!")
     });
     EventBus.$on('updatePublicChats', (publicChats) => {
       this.publicChats = publicChats;
+      console.log("Aktualizuje public chats!")
     });
+
+
     EventBus.$on('updateOnlineUsers', (onlineUsers) => {
       this.onlineUsers = onlineUsers; // Aktualizujemy listę online users
+      console.log(this.onlineUsers)
+      console.log("Aktualizuje userow")
     });
   },
 };
